a5b6caa566f0af961c7c1c375164a3eec0d65ae8
Richard Lewis <richlewis14@gmail.com>
2017-02-13 16:06:48 +0000

    adding formidable and bootstrap for form

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
-var bodyParser= require('body-parser')
+var bodyParser= require('body-parser');
+var fs = require('fs');
+var formidable = require("formidable");
+var util = require('util');
 var app = express();
 var path = require("path");
 var mongoUrl = 'mongodb://localhost:27017/touch_locations';
@@ -21,13 +24,69 @@ MongoClient.connect(mongoUrl, (err, database) => {
 
 // Form Page
 app.get('/touch_locations_form', function(req, res) {
-  res.sendFile('public/touch_location_form.html', { root : __dirname })
-})
+  displayForm(res);
+});
 
 // Post Form
 app.post('/locations', function(req, res) {
-  db.collection('touch_locations').save(req.body, (err, result) => {
+  //processAllFieldsOfTheForm(req, res);
+  processFormFieldsIndividual(req, res);
+});
+
+// Functions
+function displayForm(res) {
+    fs.readFile('public/touch_location_form.html', function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+                'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+    });
+}
+
+function processAllFieldsOfTheForm(req, res) {
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields, files) {
+        //Store the data from the fields in your data store.
+        //The data store could be a file or database or any other store based
+        //on your application.
+        res.writeHead(200, {
+            'content-type': 'text/plain'
+        });
+        res.write('received the data:\n\n');
+        res.end(util.inspect({
+            fields: fields,
+            files: files
+        }));
+    });
+}
+
+function processFormFieldsIndividual(req, res) {
+    //Store the data from the fields in your data store.
+    //The data store could be a file or database or any other store based
+    //on your application.
+    var fields = [];
+    var form = new formidable.IncomingForm();
+    form.on('field', function (field, value) {
+        console.log(field);
+        console.log(value);
+        fields[field] = value;
+    });
+
+    form.on('end', function () {
+      console.log(fields);
+      db.collection('touch_locations').save(fields, (err, result) => {
     if (err) return console.log(err)
       res.redirect('/')
-    })
-});
+    });
+        // res.writeHead(200, {
+        //     'content-type': 'text/plain'
+        // });
+        // res.write('received the data:\n\n');
+        // res.end(util.inspect({
+        //     fields: fields
+        // }));
+    });
+}
